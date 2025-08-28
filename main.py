982f25d2d686435bd504f2a89bf1f9894b9c2410
halean <halean@yahoo.com>
2025-08-28 11:59:22 +0000

    m2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from gradio_tools import gradio_tools
+
 
 from troly_dontu import troly
 
